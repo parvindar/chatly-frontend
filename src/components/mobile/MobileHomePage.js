bea3650165c1b8aef55157a91420dea7511becf5
Parvindar Singh <singh@Parvindars-MacBook-Pro.local>
2025-11-09 01:11:04 +0530

    little change

--- src/components/mobile/MobileHomePage.js
+++ src/components/mobile/MobileHomePage.js
@@ -76,15 +76,15 @@ const GroupCallContainer = styled.div`
 `;
 
 const ChatWrapper = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #2c2f33;
-  z-index: 1001;
-  display: flex;
-  flex-direction: column;
+  // position: fixed;
+  // top: 0;
+  // left: 0;
+  // right: 0;
+  // bottom: 0;
+  // background-color: #2c2f33;
+  // z-index: 1001;
+  // display: flex;
+  // flex-direction: column;
 `;
 
 const ChatHeader = styled.div`
